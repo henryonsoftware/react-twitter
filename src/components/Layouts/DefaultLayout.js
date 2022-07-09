69903ee423bebe0f35c889e8835724789f2abae5
Henry Bui <viethuongdev@gmail.com>
2022-07-09 08:30:17 +0700

    Build default layout

---
+++ src/components/Layouts/DefaultLayout.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { useStreamContext } from 'react-activity-feed'
+import classNames from 'classnames/bind'
+
+import styles from './DefaultLayout.module.scss'
+import LoadingIndicator from '../LoadingIndicator'
+import CreateTweetDialog from '../Tweet/CreateTweetDialog'
+import LeftSidebar from '../Sidebar/LeftSidebar'
+import RightSidebar from '../Sidebar/RightSidebar'
+
+const cx = classNames.bind(styles)
+
+function DefaultLayout({ children }) {
+  const { user } = useStreamContext()
+
+  const [createDialogOpened, setCreateDialogOpened] = useState(false)
+
+  if (!user) {
+    return <LoadingIndicator />
+  }
+
+  return (
+    <>
+      {createDialogOpened && <CreateTweetDialog onClickOutside={setCreateDialogOpened(false)} />}
+      <div className={cx('wrapper')}>
+        <div className={cx('content')}>
+          <div className={cx('left-sidebar')}>
+            <LeftSidebar onClickTweet={() => setCreateDialogOpened(true)} />
+          </div>
+          <main className={cx('main-content')}>{user ? children : <LoadingIndicator />}</main>
+          <div className={cx('right-sidebar')}>
+            <RightSidebar />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DefaultLayout
