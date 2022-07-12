5e173b85591f325d1530be432bc94683cfeecc29
Henry Bui <viethuongdev@gmail.com>
2022-07-12 10:48:56 +0700

    Css the follow button on the right side

--- src/components/FollowBtn.js
+++ src/components/FollowBtn.js
@@ -1,21 +1,29 @@
+import classNames from 'classnames/bind'
 import { useState } from 'react'
+import styles from './FollowBtn.module.scss'
+
+const cx = classNames.bind(styles)
 
 function FollowBtn({ userId }) {
   const [following, setFollowing] = useState(false)
+  const [buttonText, setButtonText] = useState('Following')
+  const [classHover, setClassHover] = useState('')
 
   return (
-    <div>
-      <button className={following ? 'following' : 'not-following'} onClick={() => setFollowing(!following)}>
-        {following ? (
-          <div className="follow-text">
-            <span className="follow-text__following">Following</span>
-            <span className="follow-text__unfollow">Unfollow</span>
-          </div>
-        ) : (
-          'Follow'
-        )}
-      </button>
-    </div>
+    <button
+      className={cx('button', following ? 'following' : '', classHover)}
+      onClick={() => setFollowing(!following)}
+      onMouseOver={() => {
+        setButtonText('Unfollow')
+        setClassHover('unfollow-hover')
+      }}
+      onMouseLeave={(e) => {
+        setButtonText('Following')
+        setClassHover('')
+      }}
+    >
+      {following ? buttonText : 'Follow'}
+    </button>
   )
 }
 
