644bccd761f2e211238a39b0664af2a16366548e
Henry Bui <viethuongdev@gmail.com>
2022-07-19 23:12:46 +0700

    Update avatar links

--- src/data/users.js
+++ src/data/users.js
@@ -2,7 +2,7 @@ const users = [
   {
     id: 'iamdillion',
     name: 'Dillion',
-    image: 'https://dillionmegida.com/img/deee.jpg',
+    image: 'https://i.pravatar.cc/150?u=fake@iamdillion.com',
     bio: 'Just here, doing my thing. Developer advocate at @getstream_io',
     token:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWFtZGlsbGlvbiJ9.uA8AdllQziVXJoYhtXquo_xwsnjWDMYHCBfrzZ8rTLw',
@@ -10,7 +10,7 @@ const users = [
   {
     id: 'getstream_io',
     name: 'Stream',
-    image: 'https://avatars.githubusercontent.com/u/8597527?s=200&v=4',
+    image: 'https://i.pravatar.cc/150?u=fake@getstream_io.com',
     bio: 'Deploy activity feeds and chat at scale with Stream – an API driven platform powering over a billion end users. Get started at http://getstream.io.',
     token:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2V0c3RyZWFtX2lvIn0.aKZTcOcdKE3Y8CrnUTeBpQqvfSstfQK3FNK9xMTALIU',
@@ -18,21 +18,21 @@ const users = [
   {
     id: 'jake',
     name: 'Jake',
-    image: 'https://picsum.photos/300/300',
+    image: 'https://i.pravatar.cc/150?u=fake@jake.com',
     bio: 'Just Jake, nothing much',
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFrZSJ9.hrfg6FloD6jO0g7sKuduEeyIAbWtSHTvgLd-QuHF0RQ',
   },
   {
     id: 'joe',
     name: 'Joe',
-    image: 'https://picsum.photos/200/200',
+    image: 'https://i.pravatar.cc/150?u=fake@joe.com',
     bio: 'How are you?',
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9lIn0.8c3MJQOFMBS7EcJqpJDrtjbPDBcjf90---tn4jQsjoI',
   },
   {
     id: 'mike',
     name: 'Mike',
-    image: 'https://picsum.photos/400/400',
+    image: 'https://i.pravatar.cc/150?u=fake@mike.com',
     bio: 'I am mike here. I do things on #react and #javascript',
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlrZSJ9.THPVUCfebNT_loRTcfLuIxg3p3Jnol3j8iJ5f9LT4dU',
   },
